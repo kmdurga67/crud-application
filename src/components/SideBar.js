be5c8589e6f909004cd5fd38f267d4d90c8957f6
Kumari Durga <singhdurga5698@gmail.com>
2024-04-23 17:38:25 +0530

    added css to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,17 +3,41 @@ import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <div className="bg-gray-800 text-white h-screen w-1/4 p-6">
-      <h1 className="text-2xl font-bold mb-4">Nandini Dashboard</h1>
-      <ul>
-        <li><Link to="/nandini">Nandini Dashboard</Link></li>
-        <li><Link to="/manage-customers">Manage Customers</Link></li>
-        <li><Link to="/add-food-menu">Add Food Menu</Link></li>
-        <li><Link to="/add-food">Add Food</Link></li>
-        <li><Link to="/add-events">Add Events</Link></li>
-        <li><Link to="/add-chefs">Add Chefs</Link></li>
-        <li><Link to="/manage-contacts">Manage Contacts</Link></li>
-        <li><Link to="/manage-reviews">Manage Reviews</Link></li>
+    <div className="bg-gray-800 text-white h-screen w-1/4 p-10">
+      <h1 className="text-2xl font-bold mb-16">Nandini Dashboard</h1>
+      <ul className="space-y-4">
+        <li>
+          <Link to="/nandini">Nandini Dashboard</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
+        <li>
+          <Link to="/manage-customers">Manage Customers</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
+        <li>
+          <Link to="/add-food-menu">Add Food Menu</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
+        <li>
+          <Link to="/add-food">Add Food</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
+        <li>
+          <Link to="/add-events">Add Events</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
+        <li>
+          <Link to="/add-chefs">Add Chefs</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
+        <li>
+          <Link to="/manage-contacts">Manage Contacts</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
+        <li>
+          <Link to="/manage-reviews">Manage Reviews</Link>
+          <hr className="my-4 border-gray-600" />
+        </li>
       </ul>
     </div>
   );
